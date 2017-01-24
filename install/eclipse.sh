424b8c7dac1f49dc740ebbd7c703e70e95b64f14
Gerard Krupa <GerardJohn.Krupa@homeoffice.gsi.gov.uk>
2017-01-24 12:18:19 +0000

    Made the IntelliJ and Eclipse installers consistent with checksums and preventing unnecessary re-installation

--- install/eclipse.sh
+++ install/eclipse.sh
@@ -9,29 +9,36 @@ OUTDIR=~/tools/eclipse
 CHECKSUM="7cd7c5a020bb4a8644fdaa1b4e3b1b9daab34edd17677e28b2cdd7934f2a6432320fbba241e4fb91349ae056405721522e4bfe29970a3934d49a3e8385558691"
 ICONFILE=~/.local/share/applications/eclipse.desktop
 
-mkdir -p $OUTDIR
-mkdir -p $TMPDIR
+if [ ! -x "$OUTDIR/java-neon" ]; then
 
-curl -L http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/neon/2/eclipse-java-neon-2-linux-gtk-x86_64.tar.gz -o $TMPDIR/eclipse.tar.gz
-if [ "`sha512sum $TMPDIR/eclipse.tar.gz`" == "$CHECKSUM  $TMPDIR/eclipse.tar.gz" ]; then
-    cd $OUTDIR
-    tar xvzf $TMPDIR/eclipse.tar.gz
-    rm -rf $OUTDIR/java-neon
-    rm -f $OUTDIR/current
-    mv $OUTDIR/eclipse $OUTDIR/java-neon
-    ln -s $OUTDIR/java-neon $OUTDIR/current
+    mkdir -p $OUTDIR
+    mkdir -p $TMPDIR
 
-    rm -f $ICONFILE
-    echo "[Desktop Entry]" > $ICONFILE
-    echo "Type=Application" >> $ICONFILE
-    echo "Name=Eclipse Neon" >> $ICONFILE
-    echo "Exec=$HOME/tools/eclipse/current/eclipse %u" >> $ICONFILE
-    echo "Icon=$HOME/tools/eclipse/current/icon.xpm" >> $ICONFILE
-    echo "StartupNotify=false" >> $ICONFILE
-    echo "Categories=Development;IDE;" >> $ICONFILE
-    echo "Terminal=false" >> $ICONFILE
+    curl -L http://www.mirrorservice.org/sites/download.eclipse.org/eclipseMirror/technology/epp/downloads/release/neon/2/eclipse-java-neon-2-linux-gtk-x86_64.tar.gz -o $TMPDIR/eclipse.tar.gz
+    if [ "`sha512sum $TMPDIR/eclipse.tar.gz`" == "$CHECKSUM  $TMPDIR/eclipse.tar.gz" ]; then
+        cd $OUTDIR
+        rm -rf $OUTDIR/eclipse
+        tar xvzf $TMPDIR/eclipse.tar.gz
+        rm -f $OUTDIR/current
+        mv $OUTDIR/eclipse $OUTDIR/java-neon
+        ln -s $OUTDIR/java-neon $OUTDIR/current
+
+        rm -f $ICONFILE
+        echo "[Desktop Entry]" > $ICONFILE
+        echo "Type=Application" >> $ICONFILE
+        echo "Name=Eclipse Neon" >> $ICONFILE
+        echo "Exec=$OUTDIR/current/eclipse %u" >> $ICONFILE
+        echo "Icon=$OUTDIR/current/icon.xpm" >> $ICONFILE
+        echo "StartupNotify=false" >> $ICONFILE
+        echo "Categories=Development;IDE;" >> $ICONFILE
+        echo "Terminal=false" >> $ICONFILE
+    else
+        echo "Invalid checksum for tarball.  Aborting."
+    fi
+
+    rm -r $TMPDIR
 else
-    echo "Unexpected checksum in tarball"
+    echo "Eclipse Neon is already installed"
+    echo "To remove it and re-install run rm -rf $OUTDIR/java-neon"
 fi
 
-rm -r $TMPDIR
--- install/jetbrains_toolbox.sh
+++ install/jetbrains_toolbox.sh
@@ -7,16 +7,27 @@
 VERSION=1.1.2143
 TMPDIR=~/tools/tmp
 OUTDIR=~/tools/jetbrains
+CHECKSUM="74ca89a1b97367e909075e53c67ee093ca316ecb8124bb39e7318750634552a3"
 
-mkdir -p $OUTDIR
-mkdir -p $TMPDIR
+if [ ! -x "$OUTDIR/jetbrains-toolbox-$VERSION" ]; then
+    mkdir -p $TMPDIR
 
-curl -L https://download.jetbrains.com/toolbox/jetbrains-toolbox-${VERSION}.tar.gz -o $TMPDIR/jetbrains-toolbox.tar.gz
-cd $OUTDIR
-tar xvzf $TMPDIR/jetbrains-toolbox.tar.gz
-rm -f $OUTDIR/latest
-ln -s $OUTDIR/*$VERSION* $OUTDIR/latest
+    curl -L https://download.jetbrains.com/toolbox/jetbrains-toolbox-${VERSION}.tar.gz -o $TMPDIR/jetbrains-toolbox.tar.gz
 
-rm -r $TMPDIR
+    if [ "`sha256sum $TMPDIR/jetbrains-toolbox.tar.gz`" == "$CHECKSUM  $TMPDIR/jetbrains-toolbox.tar.gz" ]; then
+        mkdir -p $OUTDIR
+        cd $OUTDIR
+        tar xvzf $TMPDIR/jetbrains-toolbox.tar.gz
+        rm -f $OUTDIR/latest
+        ln -s $OUTDIR/jetbrains-toolbox-$VERSION $OUTDIR/latest
+    else
+        echo "Invalid checksum for tarball.  Aborting."
+    fi
 
-$OUTDIR/latest/jetbrains-toolbox &
+    rm -r $TMPDIR
+
+    $OUTDIR/latest/jetbrains-toolbox &
+else
+    echo "JetBrains Toolbox $VERSION is already installed"
+    echo "To remove it, run rm -rf $OUTDIR/jetbrains-toolbox-$VERSION"
+fi
